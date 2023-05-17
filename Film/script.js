6d87469ba927273398769d24f2577fadd9f49b64
oroxxx <orox@qotur.com>
2023-05-17 10:06:14 +0400

    hazirdir

--- Film/script.js
+++ Film/script.js
@@ -1,16 +1,37 @@
-let numberOfFilms = prompt('Nece kinoya baxmisan?');
+let numberOfFilms = prompt('Neche kinoya baxmisiniz?');
 
 const personalMovieDB = {
-    count: umberOfFilms,
+    count: numberOfFilms,
     movies: {},
     actors: {},
     genres: [],
     privat: false
 }
 
-let a = prompt('Axrinci baxdiginiz film hansidir?');
-let b = prompt('Bu filmi nece balla qiymetlendirirsiniz?');
 
-personalMovieDB.movies[a] = b;
+for (let i = 0; i < 2; i++) {
+
+    let a = prompt('Axirinci baxdiginiz film');
+    let b = prompt('Nech bal ile qiymetlendirirsiniz,');
+
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB[a] = b;
+        console.log('Hazir');
+    } else {
+        console.log('Sehv var');
+        i--;
+    }
+    
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('Siz heveskasiniz');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('Siz klassik izleyicisiniz');
+} else if (personalMovieDB.count >= 30) {
+    console.log('Siz kinomensiniz');
+} else {
+    console.log('Sehv var');
+}
 
 console.log(personalMovieDB);
